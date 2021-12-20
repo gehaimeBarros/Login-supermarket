7659653a6be42c991605903fff234f4bd53cdf27
Gehaime Barros Chagas Silva <gehaime.silva@keeggo.com>
2021-12-20 00:00:11 -0300

    Login

--- home/src/js/app.js
+++ home/src/js/app.js
@@ -10,10 +10,19 @@ function viewSenha() {
 
 function viewNome() {
 
-    if (document.getElementById('viewUsuario').value == "mostrar") {
-        document.getElementById('viewUsuario').value = "ocultar";
+    if (document.getElementById('viewUsuario').value == "Mostrar") {
+        document.getElementById('viewUsuario').value = "Ocultar";
     } else {
-        document.getElementById('viewUsuario').value = "mostrar";
+        document.getElementById('viewUsuario').value = "Mostrar";
+    }
+}
+
+function btnEnter() {
+
+    if (document.getElementById('enter-usuario').value == "enter") {
+        document.getElementById('enter-usuario').value = "Ok";
+    } else {
+        document.getElementById('enter-usuario').value = "enter";
     }
 }
 
@@ -21,6 +30,9 @@ function loginUsuario() {
 
     const USER = ("admin");
     const PASS = ("admin");
+
+    const TELEFONEUSER = ("983920932");
+    const TELEFONEPASS = ("admin");
 
     var done = 0;
 
@@ -39,8 +51,16 @@ function loginUsuario() {
 
 
     }
+    if (EMAILUSUARIO == (TELEFONEUSER) && SENHAUSER == (TELEFONEPASS)) {
+        setTimeout(() => {
+            alert("Email e senha coretas!");
+            window.location.href = './home/src/html/menu.html'
+        }, 2000);
+        done = 1;
+    }
     if (done == 0) {
         alert("Dados invalidos");
+        window.location.reload();
     }
 }
 
@@ -50,4 +70,35 @@ function notPage() {
 setTimeout(notPage(), 0);
 window.onunload = function () {
     null;
+}
+
+function ClearFields() {
+
+    var RESPOSTA = prompt("Gostaria de excluir tudo?");
+
+    if (RESPOSTA == ("sim")) {
+        document.getElementById("senha-dig").value = "";
+        document.getElementById("email-usuario").value = "";
+    } else if (RESPOSTA == ("nao")) {
+        var RESPOSTA2 = prompt("Ou so email ou senha? 1 para email ou 2 para senha 3 para nenhuma.");
+
+        if (RESPOSTA2 == 1) {
+            document.getElementById("email-usuario").value = "";
+        } else if (RESPOSTA2 == 2) {
+            document.getElementById("senha-dig").value = "";
+        } else if (RESPOSTA2 == 3) {
+
+        }
+    }
+}
+
+function Data() {
+
+    var data = new Date();
+    var dias = new Array("Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado");
+    var meses = new Array("Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Outubro",
+        "Novembro", "Dezembro");
+
+    document.write(dias[data.getDay()] + ", " + data.getDate() + " de " + meses[data.getMonth()] + " de " + data
+        .getFullYear() + ".");
 }
